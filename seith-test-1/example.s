b49dc4a4d974a553202b776b8ed06dfce308a539
seith-miller <seithmiller@gmail.com>
2022-05-25 14:15:40 -0400

    add and subtract

--- seith-test-1/example.s
+++ seith-test-1/example.s
@@ -1,2 +1,21 @@
-lda $01
-sta $02
+.export Main
+.segment "CODE"
+
+.proc Main
+    ; load data into x an y
+    ldx 0
+    ldy 10
+
+    ; incrament x
+    inx
+    inx
+    inx
+
+    ; decrement y
+    dey
+    dey
+    dey
+
+    ; what is this?
+    rts
+.endproc
